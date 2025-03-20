c5d3bd39800c5a70e63611b569428e74506f9e61
User3426 <tristanrage.adresse@gmail.com>
2025-03-20 15:40:27 +0100

    Update Program.cs

--- TestNotationPolonaise/Program.cs
+++ TestNotationPolonaise/Program.cs
@@ -5,6 +5,69 @@ namespace TestNotationPolonaise
     class Program
     {
         /// <summary>
+        /// Calcule le résultat d'une expression en notation polonaise.
+        /// </summary>
+        /// <param name="formule">>La chaîne représentant l'expression en notation polonaise</param>
+        /// <returns></returns>
+        static float Polonaise(String formule)
+        {
+            try
+            {
+                float result;
+                string[] vec = formule.Split(' ');
+                int nbCases = vec.Length;
+
+                for (int k = nbCases - 1; k >= 0; k--)
+                {
+                    // Vérifier si la case contient un opérateur
+                    if (vec[k] == "+" || vec[k] == "-" || vec[k] == "*" || vec[k] == "/")
+                    {
+                        // Conversion des opérandes en float
+                        float op1 = float.Parse(vec[k + 1]);
+                        float op2 = float.Parse(vec[k + 2]);
+                        float resultat = 0;
+
+                        // Calcul en fonction de l'opérateur
+                        switch (vec[k])
+                        {
+                            case "+": resultat = op1 + op2; break;
+                            case "-": resultat = op1 - op2; break;
+                            case "*": resultat = op1 * op2; break;
+                            case "/":
+                                // Vérification pour éviter la division par zéro
+                                if (op2 == 0)
+                                    return float.NaN;
+                                else
+                                    resultat = op1 / op2;
+                                break;
+                        }
+
+                        // Remplacer l'opérateur par le résultat (converti en chaîne)
+                        vec[k] = resultat.ToString();
+
+                        // décalage du tableau
+                        for (int j = k + 1; j < nbCases - 2; j++)
+                        {
+                            vec[j] = vec[j + 2];
+                        }
+
+                        // Mettre à jour le nombre effectif de cases
+                        nbCases -= 2;
+                    }
+                }
+
+                // Si la formule n'est pas valide (par exemple, "4 3" ne contient pas d'opérateur)
+                if (nbCases != 1)
+                    return float.NaN;
+                // résultat dans la première case
+                return float.Parse(vec[0]);
+            }
+            catch
+            {
+                return float.NaN;
+            }
+        }
+        /// <summary>
         /// saisie d'une réponse d'un caractère parmi 2
         /// </summary>
         /// <param name="message">message à afficher</param>
@@ -35,7 +98,7 @@ namespace TestNotationPolonaise
             {
                 Console.WriteLine();
                 Console.WriteLine("entrez une formule polonaise en séparant chaque partie par un espace = ");
-                string laFormule = Console.ReadLine();
+                String laFormule = Console.ReadLine();
                 // affichage du résultat
                 Console.WriteLine("Résultat =  " + Polonaise(laFormule));
                 reponse = saisie("Voulez-vous continuer ?", 'O', 'N');
